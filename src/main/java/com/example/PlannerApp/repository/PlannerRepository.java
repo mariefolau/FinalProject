4fa1d258ab8f8938f6cd4406bb5485b996f4cd49
mariefolau <81399551+mariefolau@users.noreply.github.com>
2021-08-22 13:19:53 +1000

    Added repository class

--- src/main/java/com/example/PlannerApp/repository/PlannerRepository.java
+++ src/main/java/com/example/PlannerApp/repository/PlannerRepository.java
@@ -1,4 +1,10 @@
 package com.example.PlannerApp.repository;
 
-public interface PlannerRepository {
+
+import com.example.PlannerApp.model.Planner;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlannerRepository extends JpaRepository<Planner, Long> {
 }

484d0f547a6c614830f265d691a43e87a065bbfb
mariefolau <81399551+mariefolau@users.noreply.github.com>
2021-08-22 11:06:19 +1000

    First commit

---
+++ PlannerApp (2)/PlannerApp/src/main/java/com/example/PlannerApp/PlannerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.PlannerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerAppApplication.class, args);
+	}
+
+}
---
+++ PlannerApp (2)/PlannerApp/src/test/java/com/example/PlannerApp/PlannerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PlannerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

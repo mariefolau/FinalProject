fb2978d8c7d02eb28c4c483177f60ca62d74fb9e
mariefolau <81399551+mariefolau@users.noreply.github.com>
2021-08-22 13:15:42 +1000

    Added service class

--- src/main/java/com/example/PlannerApp/service/PlannerService.java
+++ src/main/java/com/example/PlannerApp/service/PlannerService.java
@@ -1,4 +1,15 @@
-package com.example.PlannerApp.service;
+package com.example.PlannerApp.Service;
+
+import com.example.PlannerApp.model.Planner;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
 
 public interface PlannerService {
+    List< Planner > getAllPlanners();
+    void savePlanner(Planner task);
+    Planner getPlannerById(long id);
+    void deletePlannerById(long id);
+    Page< Planner > findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
+    void completePlannerById(long id);
 }
